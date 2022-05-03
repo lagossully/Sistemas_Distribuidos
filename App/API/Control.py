7d5535328ccfec60a2d6a58c996cae9e1d392019
lagossully <lagosdiego97@gmail.com>
2022-05-03 00:21:51 -0400

    push previsorio redis basico

---
+++ App/API/Control.py
@@ -0,0 +1,78 @@
+import flask
+from flask import Flask, request, render_template, jsonify
+import sqlite3
+
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('home.html')
+
+
+@app.route('/api/v1/resources/books', methods=['POST'])
+def my_form_post():
+    text = request.form['text']
+    processed_text = text.upper()
+    return processed_text
+
+@app.route('/api/v1/resources/books/all', methods=['GET'])
+def api_all():
+    conn = sqlite3.connect('books.db')
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+    all_books = cur.execute('SELECT * FROM books;').fetchall()
+
+    return jsonify(all_books)
+
+
+
+@app.errorhandler(404)
+def no_encontrado(e):
+    return "<h1>404</h1><p>No se encontró la página</p><a href ='/'>Inicio</a>", 404
+
+
+@app.route('/api/v1/resources/books', methods=['GET'])
+def api_filter():
+    query_parameters = request.args
+
+    id = query_parameters.get('id')
+    published = query_parameters.get('published')
+    author = query_parameters.get('author')
+
+    query = "SELECT * FROM books WHERE"
+    to_filter = []
+
+    if id:
+        query += ' id=? AND'
+        to_filter.append(id)
+    if published:
+        query += ' published=? AND'
+        to_filter.append(published)
+    if author:
+        query += ' author=? AND'
+        to_filter.append(author)
+    if not (id or published or author):
+        return no_encontrado(404)
+
+    query = query[:-4] + ';'
+
+    conn = sqlite3.connect('books.db')
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+
+    results = cur.execute(query, to_filter).fetchall()
+
+    return jsonify(results)
+
+app.run()
+
+
--- App/API/Test.py
+++ App/API/Test.py
@@ -1,78 +1,19 @@
-import flask
-from flask import Flask, request, render_template, jsonify
-import sqlite3
+import redis
 
+r = redis.Redis(host='localhost', port=6379)
 
-app = flask.Flask(__name__)
-app.config["DEBUG"] = True
-
-def dict_factory(cursor, row):
-    d = {}
-    for idx, col in enumerate(cursor.description):
-        d[col[0]] = row[idx]
-    return d
-
-
-@app.route('/', methods=['GET'])
-def home():
-    return render_template('home.html')
-
-
-@app.route('/api/v1/resources/books', methods=['POST'])
-def my_form_post():
-    text = request.form['text']
-    processed_text = text.upper()
-    return processed_text
-
-@app.route('/api/v1/resources/books/all', methods=['GET'])
-def api_all():
-    conn = sqlite3.connect('books.db')
-    conn.row_factory = dict_factory
-    cur = conn.cursor()
-    all_books = cur.execute('SELECT * FROM books;').fetchall()
-
-    return jsonify(all_books)
+r.set("lol","league of legends")
+r.set("kotor","knigths of the old republic")
 
 
 
-@app.errorhandler(404)
-def no_encontrado(e):
-    return "<h1>404</h1><p>No se encontró la página</p><a href ='/'>Inicio</a>", 404
+print("Ingrese parametros")
+b = input()
 
-
-@app.route('/api/v1/resources/books', methods=['GET'])
-def api_filter():
-    query_parameters = request.args
-
-    id = query_parameters.get('id')
-    published = query_parameters.get('published')
-    author = query_parameters.get('author')
-
-    query = "SELECT * FROM books WHERE"
-    to_filter = []
-
-    if id:
-        query += ' id=? AND'
-        to_filter.append(id)
-    if published:
-        query += ' published=? AND'
-        to_filter.append(published)
-    if author:
-        query += ' author=? AND'
-        to_filter.append(author)
-    if not (id or published or author):
-        return no_encontrado(404)
-
-    query = query[:-4] + ';'
-
-    conn = sqlite3.connect('books.db')
-    conn.row_factory = dict_factory
-    cur = conn.cursor()
-
-    results = cur.execute(query, to_filter).fetchall()
-
-    return jsonify(results)
-
-app.run()
-
-
+if (r.exists(b)):
+    print((r.get("lol")))
+else:
+    print("no existe")
+    ###PREGUNTAR A GRPC CLIENT QUE LE DIGA A GRPC SERVER BUSCAR LOS DATOS
+    ###RECIBIR LOS DATOS Y GUARDARLOS ACÁ EN REDIS
+    ###IMPRIMIRLOS
