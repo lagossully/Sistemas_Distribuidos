1c1bf2ab2a306fdb1b630d787120de2a8065ff1c
sarvf <lucaspenailillomoreno@gemail.com>
2022-05-03 00:49:21 -0400

    update grpc 030522 00:48

--- App/grpc/DB_server.py
+++ App/grpc/DB_server.py
@@ -1,26 +1,40 @@
 
 from concurrent import futures
 import logging
+from mensaje_pb2_grpc import ItemService
+from mensaje_pb2 import Response
 
 import grpc
-import helloworld_pb2
-import helloworld_pb2_grpc
 
 
-class Greeter(helloworld_pb2_grpc.GreeterServicer):
 
-    def SayHello(self, request, context):
-        return helloworld_pb2.HelloReply(message='Hello, %s!' % request.name)
+
+class ServicioItems(ItemService):
+    def GetItem(self,request,context):
+        
+        return Response()
+    
+
+
+
+# class Greeter(helloworld_pb2_grpc.GreeterServicer):
+
+#     def SayHello(self, request, context):
+#         return helloworld_pb2.HelloReply(message='Hello, %s!' % request.name)
 
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    helloworld_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
+    # helloworld_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
     server.add_insecure_port('[::]:50051')
     server.start()
     server.wait_for_termination()
 
 
 if __name__ == '__main__':
-    logging.basicConfig()
     serve()
+
+
+
+
+
